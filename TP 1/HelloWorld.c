0d1520489170919ce394efaf3a5031462559bbda
Ugo Latry <83210143+Ugolty@users.noreply.github.com>
2023-01-25 10:48:45 +0100

    Add files via upload

---
+++ TP 1/HelloWorld.c
@@ -0,0 +1,26 @@
+/*!
+\file HelloWorld.c
+\author Ugo Latry <latryugo@cy-tech.fr>
+\version 1.0 Final
+\date 18 octobre 2022
+\brief Premier TP de programmation C
+*/
+
+/*Inclusion de librairies*/
+#include <stdio.h>
+
+/*!
+\fn int main (int argc, char** argv)
+\author Ugo Latry <latryugo@cy-tech.fr>
+\version 1.0 Final
+\date 18 octobre 2022
+\brief Hello World !
+\param arc nombre d'arguments en entree
+\return 0 si tout se passe bien
+*/
+int main (int argc, char** argv) {
+    //Affichage d'un message
+    printf("Hello World !\n");
+    //Fin du programme
+    return (0);
+}
